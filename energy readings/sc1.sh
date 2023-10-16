85bbd0f9cac805e08c47dde5ed964f4e68777dc1
samarth261 <samarth261@gmail.com>
2023-10-16 14:02:07 +0530

    Update sc1.sh

--- energy readings/sc1.sh
+++ energy readings/sc1.sh
@@ -1,1 +1,1 @@
-read energy && echo curl 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSefm44oHX5KgVcHBYKhVTy0P_ZzJCPc6A6hzQwFdueoEvt2ag/formResponse' --data "entry.1349074923=$energy"
+echo -e "Energy: \c" ; read energy && echo curl 'https://docs.google.com/forms/u/0/d/e/1FAIpQLSefm44oHX5KgVcHBYKhVTy0P_ZzJCPc6A6hzQwFdueoEvt2ag/formResponse' --data "entry.1349074923=$energy"
